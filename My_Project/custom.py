e75989c2b5b859d002ec0378f27728ad15864b64
GioLomia <lomiag@berea.edu>
2019-04-27 21:26:50 -0400

    Started to work on the project

--- My_Project/custom.py
+++ My_Project/custom.py
@@ -4,7 +4,7 @@ import numpy as np
 import cv2
 conf=config.Config()
 conf.NUM_CLASSES=81
-
+conf.BATCH_SIZE=6
 print(conf.BATCH_SIZE)
 mode="training"
 nn=model.MaskRCNN(mode,conf,"C:/Users/lomiag/PycharmProjects/Mask_RCNN_new\My_Project")
@@ -12,9 +12,13 @@ nn=model.MaskRCNN(mode,conf,"C:/Users/lomiag/PycharmProjects/Mask_RCNN_new\My_Pr
 nn.build(mode,conf)
 nn.load_weights("C:/Users/lomiag/PycharmProjects/Mask_RCNN_new/mask_rcnn_coco.h5")
 
-im1=np.array(Image.open("C:/Users/lomiag/PycharmProjects/Mask_RCNN/images/2383514521_1fc8d7b0de_z.jpg"))
-im2=np.array(Image.open("C:/Users/lomiag/PycharmProjects/Mask_RCNN/images/2502287818_41e4b0c4fb_z.jpg"))
-im_arr=np.array([im1,im2])
+im1=np.array(Image.open("C:/Users/lomiag/PycharmProjects/Mask_RCNN/images/2383514521_1fc8d7b0de_z.jpg"))/255
+im2=np.array(Image.open("C:/Users/lomiag/PycharmProjects/Mask_RCNN/images/2502287818_41e4b0c4fb_z.jpg"))/255
+im3=np.array(Image.open("C:/Users/lomiag/PycharmProjects/Mask_RCNN_new/images/7933423348_c30bd9bd4e_z.jpg"))/255
+im4=np.array(Image.open("C:/Users/lomiag/PycharmProjects/Mask_RCNN_new/images/9118579087_f9ffa19e63_z.jpg"))/255
+im5=np.array(Image.open("C:/Users/lomiag/PycharmProjects/Mask_RCNN_new/images/8053677163_d4c8f416be_z.jpg"))/255
+im6=np.array(Image.open("C:/Users/lomiag/PycharmProjects/Mask_RCNN_new/images/9247489789_132c0d534a_z.jpg"))/255
+im_arr=np.array([im1,im2,im3,im4,im5,im6])
 nn.mode="inference"
-nn.detect(im_arr)
-#
+print(im1)
+nn.detect(im1)
